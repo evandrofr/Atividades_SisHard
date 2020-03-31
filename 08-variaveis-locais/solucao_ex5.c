4ec184e9cf731f566f4da93b69d6e77cb05219bf
evandrofr <evandrofr@al.insper.edu.br>
2020-03-30 21:01:03 -0300

    ex8 ex9

--- 08-variaveis-locais/solucao_ex5.c
+++ 08-variaveis-locais/solucao_ex5.c
@@ -2,16 +2,12 @@
  int ex5_solucao(){
     int a = 1;
     int ebx = 0;
-    int eax;
     while(a > 0){
-      eax = 0;
       scanf("%d",&a);
-      eax = a;
-      if(eax > 0){
-         ebx+= eax;
+      if(a > 0){
+         ebx+= a;
       }
     }
-    eax = ebx;
-    return eax;
+    return ebx;
  }
 
