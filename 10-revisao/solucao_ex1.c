4af113e7cf9a586d7cc9acdaa0a10bf41c8e10a0
evandrofr <evandrofr@al.insper.edu.br>
2020-04-02 17:44:21 -0300

    1 e 2

--- 10-revisao/solucao_ex1.c
+++ 10-revisao/solucao_ex1.c
@@ -1,4 +1,15 @@
 
-extern <tipo> var1; //TODO: complete com o tipo correto
+extern long var1; //TODO: complete com o tipo correto
 
 //TODO: implemente a função solucao_ex1 abaixo 
+
+ 
+
+long solucao_ex1(){
+    long i=0;
+    while (var1 > 0){
+        faz_algo();
+        i++;
+    }
+    return i;
+}
--- 10-revisao/solucao_ex2.c
+++ 10-revisao/solucao_ex2.c
@@ -1,1 +1,12 @@
 //TODO: implementar função solucao_ex2 aqui
+long solucao_ex2(long *rdi, long rsi, long *rdx){
+   long rax = *rdx;
+   if(*rdi > *rdx){
+      rsi = 3*rsi;
+   }else{
+      rdx += 1;
+   }
+   *rdi = rsi + *rdx;
+   return rax;
+}
+
