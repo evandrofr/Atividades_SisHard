4ad833cbbac3bb77b47d8a90e7aa3c98656db9ca
evandrofr <evandrofr@al.insper.edu.br>
2020-04-02 18:11:17 -0300

    ex 1 2 3

--- 10-revisao/solucao_ex3.c
+++ 10-revisao/solucao_ex3.c
@@ -1,5 +1,8 @@
 // TODO: substituir tipo pelo tipo das globais
-extern <tipo> var1, var2;
+extern int var1, var2;
 
 // TODO: implementar solucao_ex3 abaixo
-
+void solucao_ex3(int *rdi){
+   *rdi = 5*var1 + 10*var2;
+   var2 = var1;
+   }
