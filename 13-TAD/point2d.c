f70fef35f2edb82b5a9bab630b7fbbdb1b4142f6
evandrofr <evandrofr@al.insper.edu.br>
2020-04-27 15:15:53 -0300

    point2D

--- 13-TAD/point2d.c
+++ 13-TAD/point2d.c
@@ -9,31 +9,47 @@ typedef struct _p Point2D;
 
 
 Point2D *point2D_new(double x, double y) {
-    return NULL;
+    Point2D *p = malloc(sizeof(Point2D));
+
+    p->x = x;
+    p->y = y;
+
+    return p;
 }
 
-
 void point2D_destroy(Point2D *p) {
-    
+    free(p);
+    p = NULL;
 }
 
 double point2D_get_x(Point2D *p) {
-    return 0;
+    return p->x;
 }
 
 double point2D_get_y(Point2D *p) {
-    return 0;
+    return p->y;
 }
 
 Point2D *point2D_add(Point2D *p1, Point2D *p2) {
-    return NULL;
+    double a = p1->x+p2->x;
+    double b = p1->y+p2->y;
+
+    Point2D *p = point2D_new(a, b);
+    
+    return p;
 }
 
 double point2D_theta(Point2D *p1, Point2D *p2) {
-    return 0;
+    double a = p1->x-p2->x;
+    double b = p1->y-p2->y;
+
+    return b/a;
 }
 
 Point2D *point2D_scale(Point2D *p, double s) {
-    return NULL;
-}
+    double a=p->x*s;
+    double b=p->y*s;
 
+    Point2D *p2 = point2D_new(a, b);
+    return p2;
+}
